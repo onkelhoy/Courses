874c268f9fcee898217d527286dd1f164c5fbf63
Henry Pap <onkelhoy@gmail.com>
2016-09-29 20:29:18 +0200

    updated search convertion

--- Workshop 2/The Happy Pirate Java/src/Helper/SearchExpression.java
+++ Workshop 2/The Happy Pirate Java/src/Helper/SearchExpression.java
@@ -1,5 +1,7 @@
 package Helper;
 
+import java.util.Calendar;
+
 /**
  * Created by henry on 2016-09-29.
  */
@@ -22,20 +24,24 @@ public class SearchExpression {
 
     static private String convertPart(String queryPart){
         String expressionPart = "";
-        boolean type = ;
-        if(checkType(queryPart, ">=")){
+        String[] arr = queryPart.split(">=");
+        if(arr.length == 2){ // >=
 
         }
-        else if(checkType(queryPart, "<=")){
+        else arr = queryPart.split("<=");
+        if(arr.length == 2){ // <=
 
         }
-        else if(checkType(queryPart, ">")){
+        else arr = queryPart.split("=");
+        if(arr.length == 2){ // =
 
         }
-        else if(checkType(queryPart, "<")){
+        else arr = queryPart.split(">");
+        if(arr.length == 2){ // >
 
         }
-        else if(checkType(queryPart, "=")){
+        else arr = queryPart.split("<");
+        if(arr.length == 2){ // <
 
         }
         else {
@@ -45,8 +51,45 @@ public class SearchExpression {
         return expressionPart;
     }
 
-    static boolean checkType(String part, String type){
-        String[] arr = part.split(type);
-        return arr.length == 2;
+    static private String getField(String fieldName, String operator, String value){
+        String field = fieldName + operator + value;
+        switch (fieldName){
+            case "gender":
+                field = "substring(identity, 10, 1) mod 2 = " + (value.equals("man") ? 1 : 0); //YYYYMMDD XXGC <- G = gender
+                break;
+            case "age":
+                field = String.format("(%s - substring(identity, 0, 5)) %s %s", Calendar.getInstance().get(Calendar.YEAR), operator, value);
+                break;
+            case "month":
+                field = "";
+                if(value.equals("jan") || value.equals("january"))  value = "01";
+                else if(value.equals("feb") || value.equals("february")) value = "02";
+                else if(value.equals("mar") || value.equals("march"))    value = "03";
+                else if(value.equals("apr") || value.equals("april"))    value = "04";
+                else if(value.equals("may"))                             value = "05";
+                else if(value.equals("june"))                            value = "06";
+                else if(value.equals("july"))                            value = "07";
+                else if(value.equals("aug") || value.equals("august"))   value = "08";
+                else if(value.equals("sep")||value.equals("september"))  value = "09";
+                else if(value.equals("oct") || value.equals("october"))  value = "10";
+                else if(value.equals("nov") || value.equals("november")) value = "11";
+                else if(value.equals("dec") || value.equals("december")) value = "12";
+                else value = "01"; //did not set propper month
+
+                field = String.format("substring(identity, 5, 2) %s %s", operator, value);
+
+                break;
+            case "boattype":
+                field = String.format("boat[@type %s '%s']", operator, value);
+                break;
+            case "boats":
+                field = "count(boats/boat)";
+                break;
+            case "boatlength":
+                field = "";
+                break;
+        }
+
+        return field;
     }
 }
