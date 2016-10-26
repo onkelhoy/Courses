556ebb5c0d6cdc897b54bc81e9e1acbe7ff61a1e
Henry Pap <onkelhoy@gmail.com>
2016-10-26 16:29:24 +0200

    soft 17 mode added

---
+++ Workshop 3/Grade 2/blackjack_java/BlackJack/model/rules/Soft17HitStrategy.java
@@ -0,0 +1,21 @@
+package BlackJack.model.rules;
+
+import BlackJack.model.Card;
+import BlackJack.model.Player;
+
+public class Soft17HitStrategy implements IHitStrategy {
+    private final int g_hitLimit = 17;
+
+    public boolean DoHit(Player a_dealer) {
+        int score = a_dealer.CalcScore();
+        if(score == g_hitLimit){ //score equals 17, check for ace and 6..
+            Iterable<Card> list = a_dealer.GetHand();
+
+            for(Card c : list){
+                if(c.GetValue() == Card.Value.Ace) return true;
+            }
+            return false;
+        }
+        else return score < g_hitLimit;
+    }
+}
